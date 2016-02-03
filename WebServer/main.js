a4daf6fc6c0236c32213c063f7fb5c35068a5c1d
Alvin Lim <alim459@gmail.com>
2016-02-02 22:10:26 -0800

    initial commit with MQTT functionality working

--- WebServer/main.js
+++ WebServer/main.js
@@ -5,3 +5,38 @@
  */
 
 
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//to run, you will need to npm install mqtt -g (sudo access might be required
+var mqtt = require('mqtt');
+var options = {
+    keepalive: 0,
+//    port: 800
+}
+
+
+console.log('Started MQTT Websocket');
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://broker.mqttdashboard.com', options);
+ 
+client.on('connect', function () {
+  client.subscribe('team-mat');
+  console.log('connected');
+});
+ 
+ //called when a message event is received
+client.on('message', function (topic, message) {
+  // message is Buffer 
+  console.log(message.toString());
+  var messageJSON = JSON.parse(message.toString());
+  
+  if (messageJSON.hasOwnProperty("status") && messageJSON.status === "disconnect"){
+      console.log("Intention to exit was logged");
+      client.end();
+  }
+});
