20f356bc7290c95d17fd0f0ead06c526a294e04d
lijtong <ljtong1@gmail.com>
2016-03-03 21:27:20 -0500

    Fixed socket implementation and added in battery query, battery life can now be plotted, still needs to be cleaned up

--- Dashboard/js/battery.js
+++ Dashboard/js/battery.js
@@ -2,6 +2,7 @@
 //input: '{"clientID":String, "datetime":Array[], "battery":Array[]}'
 //output: battery percentage graph
 function batteryDisplay(data){
+	console.log(data);
 	var margin = {
 		top: 20,
 		right: 50,
@@ -13,6 +14,9 @@ function batteryDisplay(data){
 
 	if(isJSON(data)){
 		var data = JSON.parse(data);
+		//console.log(data);
+		var data = data.hits.hits;
+		//console.log("Battery life = ");
 		console.log(data);
 		// var clientId = data.clientId;
 		// var rDateTime = data.dateTimeRec;
@@ -23,13 +27,13 @@ function batteryDisplay(data){
 		//earliest date
 		// var xMin = new Date(d3.min(rDateTime,function(d, i){
 		var xMin = new Date(parseInt(d3.min(data,function(d, i){
-				return d.datetime;
+				return d.fields.datetime;
 				})));
-		// console.log(xMin);
+		 console.log(xMin);
 		//latest date
 		// var xMax = new Date(d3.max(rDateTime,function(d, i){
 		var xMax = new Date(parseInt(d3.max(data,function(d, i){
-				return d.datetime;
+				return d.fields.datetime;
 				})));
 		// console.log(xMax);
 
@@ -37,7 +41,7 @@ function batteryDisplay(data){
 		var x = d3.time.scale()
 			// .domain([xMin, xMax])
 			.domain(d3.extent(data,function(d){
-				return d.datetime;
+				return d.fields.datetime;
 			}))
 			.range([0, width]);
 		//y axis (percentage)
@@ -64,11 +68,11 @@ function batteryDisplay(data){
 		var line = d3.svg.line()
 			.x(function(d){
 				// console.log(x(new Date(parseInt(d.datetime))));
-				return x(new Date(parseInt(d.datetime)));
+				return x(new Date(parseInt(d.fields.datetime)));
 			})
 			.y(function(d){
 				// console.log(y(d.battery));
-				return y(d.battery);
+				return y(d.fields.battery);
 			})
 			.interpolate("line");
 
