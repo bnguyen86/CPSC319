2877501a985365c3a01e65216233fdae911da784
Douglas Wong <fight320@hotmail.com>
2016-03-04 14:23:40 -0800

    started to work on super secret stuff; not for demo

--- Dashboard/js/maps.js
+++ Dashboard/js/maps.js
@@ -5,5 +5,31 @@
 //			where __n is the total number of data points
 //OUTPUT: map with user
 function mapDisplay(data){
-	return(data);
-}
+	var margin = {
+		top: 20,
+		right: 50,
+		bottom: 30,
+		left: 50
+		};
+	var width = 1000 - margin.left - margin.right;
+	var height = 300 - margin.top - margin.bottom;
+
+	var svg = d3.select('#history')
+			.append('svg')
+			.attr('width', width + margin.left + margin.right)
+			.attr('height', height + margin.top + margin.bottom)
+			.attr('transform','translate('+margin.left+','+margin.top+')');
+
+	// initialize();
+};
+
+function initialize(){
+	var mapProp = {
+	center: new google.maps.LatLng(51.508742, -0.120850),
+	zoom: 5,
+mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+var map = new google.maps.Map(document.getElementById("history"), mapProp);
+};
+
+google.maps.event.addDomListener(window, 'load', initialize);
