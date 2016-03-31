9d29841aad4debe815d79ea4f369a8cecccb3703
TheSchnoo <wes.berry1@gmail.com>
2016-03-30 21:41:17 -0700

    Update to add name test 9

--- WebServer/main.js
+++ WebServer/main.js
@@ -168,7 +168,7 @@ io.on('connection', function(socket) {
                     "terms": {
                         "field": "clientId"
                     }
-                }
+                },
                 "name": {
                     "terms": {
                         "field": "clientName"
